3b0b12c8798322a44d2148741c5e15b31a457088
Gatikrushna Sahu <gatikrushna.sahu@in.pega.com>
2017-05-14 23:05:13 +0530

    linked list

--- src/com/gati/dsalgo/linkedlist/SinglyLinkedList.java
+++ src/com/gati/dsalgo/linkedlist/SinglyLinkedList.java
@@ -9,11 +9,25 @@ package com.gati.dsalgo.linkedlist;
  * 
  *
  */
+/**
+ * @author sahug
+ *
+ * @param <E>
+ */
+/**
+ * @author sahug
+ *
+ * @param <E>
+ */
 public class SinglyLinkedList<E> {
 
 	private Node<E> head;
 
 	private int size = 0;
+
+	public int getSize() {
+		return size;
+	}
 
 	/**
 	 * Inserts the specified element at the beginning of this list.
@@ -60,7 +74,64 @@ public class SinglyLinkedList<E> {
 			}
 			head.next = newNode;
 		}
+		size++;
+	}
 
+	/**
+	 * Given a key, deletes the first occurrence of key in linked list.
+	 * 
+	 * @param key
+	 */
+	public void deleteNode(E key) {
+		Node<E> tempNode = head;
+		Node<E> previousNode = null;
+		if (head == null) {
+			return;
+		}
+		if (tempNode != null && tempNode.data == key) {
+			head = tempNode.next;
+			return;
+		}
+		while (tempNode != null && tempNode.data != key) {
+			previousNode = tempNode;
+			tempNode = tempNode.next;
+		}
+		// If key was not present in linked list
+		if (tempNode == null)
+			return;
+
+		// Unlink the node from linked list
+		previousNode.next = tempNode.next;
+		size--;
+	}
+
+	public void deleteNodeFrom(int position) {
+		if (position == 0) {
+			head.next = head;
+		}
+		Node tnode = head;
+		Node prevNode = null;
+		int count = 0;
+		while (tnode != null && tnode.next != null) {
+			count++;
+			if (position == count) {
+				prevNode.next = tnode.next;
+			}
+			prevNode = tnode;
+			tnode = tnode.next;
+		}
+	}
+	
+	public void reverse(){
+		Node prevNode=null;
+		Node current=head;
+		while(current !=null){
+			Node tempNode=current.next;
+			current.next=prevNode;
+			prevNode=current;
+			current=tempNode;
+		}
+		head=prevNode;
 	}
 
 	/**
@@ -89,6 +160,33 @@ public class SinglyLinkedList<E> {
 			System.out.print(tnode.data + " ");
 			tnode = tnode.next;
 		}
+	}
+
+	/**
+	 * 1. Initialize count = 0 2. Loop through the link list a. if count is
+	 * equal to the passed index then return current node. b. Increment count
+	 * c.change current to point to next of the current.
+	 * 
+	 * @param index
+	 * @param llist
+	 * @return
+	 */
+	public E getNthNode(int index, SinglyLinkedList<Integer> llist) {
+		int size = llist.getSize();
+		Node<E> tempNode = head;
+		if (index > size) {
+			return null;
+		}
+		int count = 1;
+		while (tempNode != null) {
+			if (count == index) {
+				return tempNode.data;
+			}
+			tempNode = tempNode.next;
+			count++;
+		}
+		return null;
+
 	}
 
 }
